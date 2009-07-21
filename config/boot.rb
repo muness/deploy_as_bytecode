4e487dd9d0b447f945934f8d9fbbbab9369e1cb4
Muness Alrubaie <muness@thinkrelevance.com>
2009-07-21 17:40:32 -0400

    show error if not running inside jruby

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,8 @@
 # Don't change this file!
 # Configure your app in config/environment.rb and config/environments/*.rb
 
+raise "try again with jruby" unless defined?(JRUBY_VERSION)
+
 RAILS_ROOT = "#{File.dirname(__FILE__)}/.." unless defined?(RAILS_ROOT)
 
 module Rails
